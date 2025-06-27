baefc9d44e21ce8cd5199cdbf709dd15daee9301
scocuro <hyunseo.kang238@gmail.com>
2025-06-27 09:54:34 +0900

    Update real_estate_report.py

--- real_estate_report.py
+++ real_estate_report.py
@@ -82,7 +82,7 @@ if not API_KEY:
 
 BASE_URL = (
     'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/'
-    'service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
+    'service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade'
 )
 
 def fetch_transactions(lawd_cd: str, deal_ym: str, page_no: int) -> pd.DataFrame:
